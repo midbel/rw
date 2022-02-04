e60551cbb7bcde52eeaa06c69bdc5c79533d3722
midbel <brun.nicolas21@gmail.com>
2022-02-04 09:54:57 +0100

    gofmt

--- rw.go
+++ rw.go
@@ -1,8 +1,12 @@
 package rw
 
 import (
+  "errors"
   "io"
+  "os"
 )
+
+var ErrTooLong = errors.New("rw: write too long")
 
 type LimitedWriter struct{
   W io.Writer
@@ -19,5 +23,48 @@ func LimitWriter(w io.Writer, n int64) io.Writer {
 }
 
 func (w *LimitedWriter) Write(b []byte) (int, error) {
+  if w.r <= 0 {
+    return 0, ErrTooLong
+  }
+
   return 0, nil
+}
+
+var zeroes = make([]byte, 4096)
+
+type zero struct{}
+
+func Zero() io.Reader {
+  return zero{}
+}
+
+func (_ zero) Read(b []byte) (int, error) {
+  var n int
+  for n < len(b) {
+    x := copy(b[n:], zeroes)
+    n += x
+  }
+  return n, nil
+}
+
+type Pipe struct {
+  R *os.File
+  W *os.File
+}
+
+func PipeFile() (*Pipe, error) {
+  var (
+    p Pipe
+    err error
+  )
+  p.R, p.W, err = os.Pipe()
+  return &p, err
+}
+
+func (p *Pipe) Read(b []byte) (int, error) {
+  return p.R.Read(b)
+}
+
+func (p *Pipe) Write(b []byte) (int, error) {
+  return p.W.Write(b)
 }
